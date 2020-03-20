07ff768e05c4807d867f14e8d5749346f9a0808d
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-03-20 12:24:48 -0400

    added banner

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,12 @@ import HomeSecondBanner from './HomeSecondBanner';
 import Languages from './Languages';
 import HomeContact from './HomeContact';
 import Projects from './Projects';
+import HomeMainBanner from './HomeMainBanner';
 
 function Home(){
     return(
         <div>
-            
+            <HomeMainBanner/>
             <HomeSecondBanner/>
             <Projects />
             <Languages/>
---
+++ src/Components/HomeMainBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomeMainBanner(){
+    return(
+        <div className="mainBanner">
+            <div className="mainBannerTitle">
+                Alex Battle
+            </div>
+            <div className="mainBannerSub">
+                Man. Developer. Data Dog.
+            </div>
+        </div>
+    )
+}
+export default HomeMainBanner;

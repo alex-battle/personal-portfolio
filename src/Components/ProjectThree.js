8d1b7e68898789ee10ff063ce68805571ec21305
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-03-20 09:32:33 -0400

    corrected links

--- src/Components/ProjectThree.js
+++ src/Components/ProjectThree.js
@@ -11,7 +11,7 @@ function ProjectThree() {
                  Uses React.js and Foursquare's API to search local bars by category and pull up 5-10 top bars. Sort them by Business name, address, hours on one page. Switching between different routes(Home page, bar type, list, and actual bar page). I built this in 3 days.
             <div className="projectLinks">
                     <a href='http://budget-boyz.herokuapp.com'>View Site</a>
-                    <a href='https://alex-battle.github.io/Budget/'>Github</a>
+                    <a href='https://github.com/alex-battle/Bar-Tap'>Github</a>
                 </div>
             </div>
             <div className="projectThree">

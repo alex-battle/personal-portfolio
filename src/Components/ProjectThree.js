098ff287bc8499f19b61d36276c9b9d3930cf13a
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-03-23 08:58:03 -0400

    updated bartap website

--- src/Components/ProjectThree.js
+++ src/Components/ProjectThree.js
@@ -10,7 +10,7 @@ function ProjectThree() {
                 </div>
                  Uses React.js and Foursquare's API to search local bars by category and pull up 5-10 top bars. Sort them by Business name, address, hours on one page. Switching between different routes(Home page, bar type, list, and actual bar page). I built this in 3 days.
             <div className="projectLinks">
-                    <a href='http://budget-boyz.herokuapp.com'>View Site</a>
+                    <a href='https://bartap.surge.sh'>View Site</a>
                     <a href='https://github.com/alex-battle/Bar-Tap'>Github</a>
                 </div>
             </div>

7128335320ca228bb3c3e08a282f5263ac2f0dee
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-03-27 12:54:54 -0400

    update

--- src/Components/HomeSecondBanner.js
+++ src/Components/HomeSecondBanner.js
@@ -6,47 +6,47 @@ AOS.init();
 
 function HomeSecondBanner() {
     return (
-        <div data-aos="fade-up"
-            // data-aos="fade-down"
-            data-aos-offset="200"
-            data-aos-delay="10"
-            data-aos-duration="700"
-            data-aos-easing="ease-in-out"
-            data-aos-mirror="false"
-            // data-aos-once="false"
-            // data-aos-anchor-placement="top-center"
-            > 
         <div className="homeSecondBanner">
-            <div className="whoAmI">
-                Who is Alex Battle?
+            <div data-aos="fade-up"
+            // data-aos="fade-down"
+                data-aos-offset="200"
+                data-aos-delay="10"
+                data-aos-duration="700"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="false"
+        // data-aos-once="false"
+        // data-aos-anchor-placement="top-center"
+            >
+                <div className="whoAmI">
+                    Who is Alex Battle?
+                </div>
+                <div className="secondBannerDescription">
+                    <div className="fadingEffect"></div>
+                        Full stack web developer with a passion for
+                        learning new technologies and the continual
+                        improvement of myself and those around me. I
+                        am a motivated young professional with an
+                        aptitude for innovation and creative
+                        problem-solving. I bring strong skills in
+                        critical analysis, teamwork and project management
+                        that help companies create an environment that focuses
+                        on productivity and positivity. With my history as a
+                        real estate agent, I bring an unmatched drive and
+                        persistence with me as a self-starter as well as an equal
+                        ability to add value as a team member whether it be a
+                        large or small role. I am always looking for opportunities
+                        to leverage my unique set of experiences, I relish the chance
+                        to put them to use in new ways.
+                    </div>
+                    <div className="aboutMeButton">
+                        <a href='/about' className="button">Learn More</a>
+                    {/* <a href='/about'><button>Learn more</button></a> */}
+                    </div>
+                {/* <div className="aboutMeButtonHolder">
+                <a href='/about'><button>Learn more</button></a>
+                </div> */}
             </div>
-            <div className="secondBannerDescription">
-                <div className="fadingEffect"></div>
-                Full stack web developer with a passion for
-                learning new technologies and the continual
-                improvement of myself and those around me. I
-                am a motivated young professional with an
-                aptitude for innovation and creative
-                problem-solving. I bring strong skills in
-                critical analysis, teamwork and project management
-                that help companies create an environment that focuses
-                on productivity and positivity. With my history as a
-                real estate agent, I bring an unmatched drive and
-                persistence with me as a self-starter as well as an equal
-                ability to add value as a team member whether it be a
-                large or small role. I am always looking for opportunities
-                to leverage my unique set of experiences, I relish the chance
-                to put them to use in new ways.
-            </div>
-            <div className="aboutMeButton">
-            <a href='/about' className="button">Learn More</a>
-                {/* <a href='/about'><button>Learn more</button></a> */}
-            </div>
-            {/* <div className="aboutMeButtonHolder">
-            <a href='/about'><button>Learn more</button></a>
-            </div> */}
-            </div>
-        // </div>
+        </div>
     )
 }
 export default HomeSecondBanner;
--- src/Components/Languages.js
+++ src/Components/Languages.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
 
-function Languages(){
-    return(
+function Languages() {
+    return (
         <div className="languages">
             <div className="techBanner">
                 <div className="techBannerTitle">Technical Skills</div>
             </div>
-            <div className="languageIcons">
-            <div className="ruby">
-            <img src="https://img.icons8.com/wired/100/000000/ruby-programming-language.png"/>
-            RUBY
-            </div>
-            <div className="react">
-            <img src="https://img.icons8.com/wired/100/000000/react.png"/>
-            REACT
-            </div>
-            <div className="css">
-            <img src="https://img.icons8.com/ios/100/000000/css.png"/>
-            CSS
-            </div>
-            <div className="mysql">
-            <img src="https://img.icons8.com/ios/100/000000/mysql.png"/>
-            MYSQL
-            </div>
-            <div className="javascript">
-            <img src="https://img.icons8.com/ios-filled/100/000000/javascript-logo.png"/>
-            JAVASCRIPT
-            </div>
-            <div className="html">
-            <img src="https://img.icons8.com/ios/100/000000/html.png"/>
-            HTML
-            </div>
+            <div data-aos="fade-right"
+                // data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="5"
+                data-aos-duration="200"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+            // data-aos-once="false"
+            // data-aos-anchor-placement="top-center"
+            >
+                <div className="languageIcons">
+                    <div className="ruby">
+                        <img src="https://img.icons8.com/wired/100/000000/ruby-programming-language.png" />
+                        RUBY
+                    </div>
+                    <div className="react">
+                        <img src="https://img.icons8.com/wired/100/000000/react.png" />
+                        REACT
+                    </div>
+                    <div className="css">
+                        <img src="https://img.icons8.com/ios/100/000000/css.png" />
+                        CSS
+                    </div>
+                    <div className="mysql">
+                        <img src="https://img.icons8.com/ios/100/000000/mysql.png" />
+                        MYSQL
+                    </div>
+                    <div className="javascript">
+                        <img src="https://img.icons8.com/ios-filled/100/000000/javascript-logo.png" />
+                        JAVASCRIPT
+                    </div>
+                    <div className="html">
+                        <img src="https://img.icons8.com/ios/100/000000/html.png" />
+                        HTML
+                    </div>
+                </div>
             </div>
 
         </div>
